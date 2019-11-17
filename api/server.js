35018808feafd1a762cba0a0be6cd9918a22ccc2
unknown <neduvinci@gmail.com>
2019-11-17 16:24:22 +0100

    set up server and index.js

---
+++ api/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+module.exports = server
