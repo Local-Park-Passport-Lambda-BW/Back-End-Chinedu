54e0d15ca4621d51c789b5b87305bcf61b086e6d
unknown <neduvinci@gmail.com>
2019-11-17 16:39:55 +0100

    created park table and db

--- database/migrations/20191117162540_create-park-table.js
+++ database/migrations/20191117162540_create-park-table.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('park', tbl => {
+        tbl.string('name').notNullable();
+        tbl.string('city').notNullable();
+        tbl.string('state').notNullable();
+        tbl.string('country').notNullable();
+        tbl.string('description').notNullable();
+        tbl.primary(['name', 'city']);
+  })
 };
 
 exports.down = function(knex) {
-  
+    return knex
+        .schema
+        .dropTableIfExists('park');
 };
