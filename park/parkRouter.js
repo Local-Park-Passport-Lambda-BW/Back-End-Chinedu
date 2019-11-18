995c7de76df848825a64010a65ea76aee8390077
unknown <neduvinci@gmail.com>
2019-11-18 04:23:01 +0100

    set up test and endpoint to add park. Also shows same park cannot be added twice

--- park/parkRouter.js
+++ park/parkRouter.js
@@ -7,4 +7,22 @@ router.get('/', (req, res) => {
     res.status(200).json({message: "Hi"})
 })
 
+router.post('/', (req, res) => {
+    const parkDetails = req.body;
+    db.addPark(parkDetails)
+        .then(newPark => {
+            res
+                .status(201)
+                .json(newPark)
+        })
+        .catch(error => {
+            res
+                .status(500)
+                .json({
+                    message: "Failure to add park",
+                    error: error
+                })
+        })
+})
+
 module.exports = router
--- park/parkRouter.test.js
+++ park/parkRouter.test.js
@@ -6,15 +6,89 @@ beforeEach(() => {
     return db('park').truncate();
 })
 
-describe('Router connection test', () => {
-    it('returns the correct status', async () => {
-        const res = await request(server).get('/api/parks');
-        expect(res.status).toBe(200)
+describe('Park router', () => {
+
+    describe('Router connection test', () => {
+        it('returns the correct status of 200', async () => {
+            const res = await request(server)
+                .get('/api/parks');
+            expect(res.status).toBe(200)
+        })
+    
+        it('returns the correct message', async () => {
+            const res = await request(server).get('/api/parks');
+            expect(res.type).toBe('application/json');
+            expect(res.body).toEqual({message: "Hi"})
+        })
     })
 
-    it('returns the correct message', async () => {
-        const res = await request(server).get('/api/parks');
-        expect(res.type).toBe('application/json');
-        expect(res.body).toEqual({message: "Hi"})
+    describe('POST /', () => {
+
+        it('returns status of 201', async () => {
+            const parkDetails = {
+                name: 'Pleasure Park',
+                city: 'PHC',
+                state: 'Rivers',
+                country: 'Nigeria',
+                description: 'A fun place to be'
+            }
+            const res = await request(server)
+                .post('/api/parks')
+                .send(parkDetails);
+            expect(res.status).toBe(201)
+        })
+        
+        it('returns the new park after adding', async () => {
+            const parkDetails = {
+                name: 'Pleasure Park',
+                city: 'PHC',
+                state: 'Rivers',
+                country: 'Nigeria',
+                description: 'A fun place to be'
+            }
+            const res = await request(server)
+            .post('/api/parks')
+            .send(parkDetails);
+            expect(res.type).toBe('application/json')
+            expect(res.body).toEqual(parkDetails)
+        })
+        
+        it('returns status of 500 when adding a park 2nd time', async () => {
+            const parkDetails = {
+                name: 'Pleasure Park',
+                city: 'PHC',
+                state: 'Rivers',
+                country: 'Nigeria',
+                description: 'A fun place to be'
+            }
+            const res = await request(server)
+            .post('/api/parks')
+            .send(parkDetails);
+            const res2 = await request(server)
+            .post('/api/parks')
+            .send(parkDetails);
+            expect(res2.status).toBe(500)
+        })
+
+        it('returns error message when adding a park 2nd time', async () => {
+            const parkDetails = {
+                name: 'Pleasure Park',
+                city: 'PHC',
+                state: 'Rivers',
+                country: 'Nigeria',
+                description: 'A fun place to be'
+            }
+            const res = await request(server)
+                .post('/api/parks')
+                .send(parkDetails);
+            
+            const res2 = await request(server)
+                .post('/api/parks')
+                .send(parkDetails);
+            
+            expect(res2.type).toBe('application/json')
+            expect(res2.body.message).toBe('Failure to add park')
+        })
     })
+    
 })
