03ad63c2f7b8915dd3812b401b3888f5bb7fc6d5
unknown <neduvinci@gmail.com>
2019-11-19 11:23:30 +0100

    provided alternatives to the env variables

--- auth/generateToken.js
+++ auth/generateToken.js
@@ -10,7 +10,7 @@ module.exports = (user) => {
     }
     const result = jwt.sign(
         payload,
-        process.env.JWT_SECRET || 'me secret me',
+        process.env.JWT_SECRET || 'them secret them',
         options
     )
     return result
