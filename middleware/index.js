f0d8cd8aca99484aba7921b20c4d0705ec3f3f9c
unknown <neduvinci@gmail.com>
2019-11-19 20:41:34 +0100

    added validatePark middleware, updated validateParkDetails and validateUserDetails middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,12 +1,37 @@
+const parkDb = require('../park/parkModel')
 
 module.exports = {
     validateUserdetails,
     validateParkDetails,
+    validatePark,
+}
+
+async function validatePark(req, res, next) {
+    const { id } = req.params
+    const park = await parkDb.findById(id)
+    if (park) {
+        req.park = park
+        next()
+    }
+    else {
+        res
+            .status(404)
+            .json({
+                message: "There is no park with id " + id
+            })
+    }
 }
 
 function validateParkDetails(req, res, next) {
     const { name, city, country, description } = req.body
-    if (name && city && country && description) {
+    if (!Object.keys(req.body).length) {
+        res
+            .status(400)
+            .json({
+                message: "Missing park details"
+            })
+    }
+    else if (name && city && country && description) {
         next()
     }
     else {
@@ -21,7 +46,14 @@ function validateParkDetails(req, res, next) {
 function validateUserdetails(req, res, next) {
     if (req.path === '/register') {
         const { name, username, email, password } = req.body;
-        if (name && username && email && password) {
+        if (!Object.keys(req.body).length) {
+            res
+                .status(400)
+                .json({
+                    message: "Missing user details"
+                })
+        }
+        else if (name && username && email && password) {
             next()
         }
         else {
@@ -34,7 +66,14 @@ function validateUserdetails(req, res, next) {
     }
     if (req.path === '/login') {
         const { username, email, password } = req.body;
-        if ((username || email) && password) {
+        if (!Object.keys(req.body).length) {
+            res
+                .status(400)
+                .json({
+                    message: "Missing user details"
+                })
+        }
+        else if ((username || email) && password) {
             next()
         }
         else {
