9f296a093a91e4e2c0342c5ac0ffc054adf1d5a6
unknown <neduvinci@gmail.com>
2019-11-20 10:14:12 +0100

    moved register and login endpoints from parkRouter to userRouter

---
+++ routes/userRouter.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const bcrypt = require('bcryptjs')
+const { validateUserdetails } = require('../middleware')
+const dbUser = require('../user/userModel')
+const generateToken = require('../auth/generateToken')
+
+const router = express.Router()
+
+router.post('/register', validateUserdetails, (req, res) => {
+    let userDetails = req.body
+    const hash = bcrypt.hashSync(userDetails.password, 10)
+    userDetails = { ...userDetails, password: hash }
+    dbUser.addUser(userDetails)
+        .then(newUser => {
+            res
+                .status(201)
+                .json(newUser)
+        })
+        .catch(error => {
+            res
+                .status(500)
+                .json({
+                    message: "Failure to create new user",
+                    error: error
+            })
+        })
+})
+
+router.post('/login', validateUserdetails, (req, res) => {
+    const loginName = req.body.username || req.body.email
+    const password = req.body.password
+    dbUser.findByUsernameOrEmail(loginName)
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = generateToken(user);
+                res
+                    .status(200)
+                    .json({
+                        message: "Welcome " + user.name,
+                        token: token
+                    })
+            }
+            else {
+                res
+                    .status(401)
+                    .json({
+                        message: "Invalid credentials"
+                    })
+            }
+        })
+        .catch(error => {
+            res
+                .status(500)
+                .json({
+                    message: "Login failure",
+                    error: error
+            })
+        })
+})
+
+module.exports = router
